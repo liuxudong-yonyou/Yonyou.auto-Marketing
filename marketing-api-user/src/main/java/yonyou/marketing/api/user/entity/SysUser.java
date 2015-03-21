e17b4b09743320f1b5e71fab4c9f11dd45f4fdeb
liuxudong <liuxudong@youyon.com>
2015-03-21 22:30:13 +0800

    测试代码的提交

--- marketing-api-user/src/main/java/yonyou/marketing/api/user/entity/SysUser.java
+++ marketing-api-user/src/main/java/yonyou/marketing/api/user/entity/SysUser.java
@@ -3,7 +3,7 @@ package yonyou.marketing.api.user.entity;
 
 public class SysUser {
 	
-		private Integer id;//   id主键	private String email;//   邮箱也是登录帐号	private String pwd;//   登录密码	private String nickName;//   昵称	private Integer state;//   状态 0=可用,1=禁用	private Integer loginCount;//   登录总次数	private java.sql.Timestamp loginTime;//   最后登录时间	private Integer deleted;//   删除状态 0=未删除,1=已删除	private java.sql.Timestamp createTime;//   创建时间	private java.sql.Timestamp updateTime;//   修改时间	private Integer createBy;//   创建人	private Integer updateBy;//   修改人
+		private Integer id;//   id主键	private String email;//   邮箱也是登录帐号 email	private String pwd;//   登录密码	private String nickName;//   昵称	private Integer state;//   状态 0=可用,1=禁用	private Integer loginCount;//   登录总次数	private java.sql.Timestamp loginTime;//   最后登录时间	private Integer deleted;//   删除状态 0=未删除,1=已删除	private java.sql.Timestamp createTime;//   创建时间	private java.sql.Timestamp updateTime;//   修改时间	private Integer createBy;//   创建人	private Integer updateBy;//   修改人
 	
 	private Integer superAdmin;//超级管理员
 	
